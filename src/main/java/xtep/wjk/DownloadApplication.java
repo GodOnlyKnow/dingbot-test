44e85d0dbd3509508c21f355fd8437ac413a3e84
WJK <a523782329@126.com>
2017-02-28 18:28:08 +0800

    我有一句MMP

--- src/main/java/xtep/wjk/DownloadApplication.java
+++ src/main/java/xtep/wjk/DownloadApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DownloadApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DownloadApplication.class, args);
 	}
 }
