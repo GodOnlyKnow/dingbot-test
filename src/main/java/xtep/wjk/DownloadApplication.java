66d8ca7472e8c5d12b1119d78290a0d4b6e490d7
WJK <a523782329@126.com>
2017-02-28 18:30:43 +0800

    分支测试 dev

--- src/main/java/xtep/wjk/DownloadApplication.java
+++ src/main/java/xtep/wjk/DownloadApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DownloadApplication {
 
 	public static void main(String[] args) {
-
+        // Mains Start
 		SpringApplication.run(DownloadApplication.class, args);
 	}
 }
